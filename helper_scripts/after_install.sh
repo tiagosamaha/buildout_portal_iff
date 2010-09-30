531be0356fe7fbf8b13eda6eb0b12a67f5b1b1fc
Herman Caldara <hermancaldara@gmail.com>
2010-09-29 23:22:38 -0300

    Consertado o script "after_install.sh"

--- helper_scripts/after_install.sh
+++ helper_scripts/after_install.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
+CURRENT_DIRECTORY=`pwd`
 PLONE_HOME=/usr/local/Plone
 ZEOCLUSTER_HOME=zeocluster
 
-`cp -r $PLONE_HOME/$ZEOCLUSTER_HOME/after_install/sd $PLONE_HOME/$ZEOCLUSTER_HOME/parts/client1/lib/python`
-`cp $PLONE_HOME/$ZEOCLUSTER_HOME/after_install/sd-configure.zcml $PLONE_HOME/$ZEOCLUSTER_HOME/parts/client1/etc/package-includes`
+`cp -r $CURRENT_DIRECTORY/after_install/sd $PLONE_HOME/$ZEOCLUSTER_HOME/parts/client1/lib/python`
+`cp $CURRENT_DIRECTORY/after_install/sd-configure.zcml $PLONE_HOME/$ZEOCLUSTER_HOME/parts/client1/etc/package-includes`
 
-`cp -r $PLONE_HOME/$ZEOCLUSTER_HOME/after_install/sd $PLONE_HOME/$ZEOCLUSTER_HOME/parts/client2/lib/python`
-`cp $PLONE_HOME/$ZEOCLUSTER_HOME/after_install/sd-configure.zcml $PLONE_HOME/$ZEOCLUSTER_HOME/parts/client2/etc/package-includes`
+`cp -r $CURRENT_DIRECTORY/after_install/sd $PLONE_HOME/$ZEOCLUSTER_HOME/parts/client2/lib/python`
+`cp $CURRENT_DIRECTORY/after_install/sd-configure.zcml $PLONE_HOME/$ZEOCLUSTER_HOME/parts/client2/etc/package-includes`
 
-`cp -r $PLONE_HOME/$ZEOCLUSTER_HOME/after_install/sd $PLONE_HOME/$ZEOCLUSTER_HOME/parts/client3/lib/python`
-`cp $PLONE_HOME/$ZEOCLUSTER_HOME/after_install/sd-configure.zcml $PLONE_HOME/$ZEOCLUSTER_HOME/parts/client3/etc/package-includes`
+`cp -r $CURRENT_DIRECTORY/after_install/sd $PLONE_HOME/$ZEOCLUSTER_HOME/parts/client3/lib/python`
+`cp $CURRENT_DIRECTORY/after_install/sd-configure.zcml $PLONE_HOME/$ZEOCLUSTER_HOME/parts/client3/etc/package-includes`
 
-`cp $PLONE_HOME/$ZEOCLUSTER_HOME/after_install/sitecustomize.py $PLONE_HOME/Python-2.4/lib/python2.4`
+`cp $CURRENT_DIRECTORY/after_install/sitecustomize.py $PLONE_HOME/Python-2.4/lib/python2.4`
